cb03f1016069e7144bff02d25e01e19fd0390dd5
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-04-09 22:10:54 +0530

    added honme get response

--- Express/app.js
+++ Express/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get("/" , (req,res)=>{
     res.send("this is my first express app.");
@@ -11,7 +11,11 @@ app.get("/about" , (req,res)=>{
 });
 
 app.post("/about" , (req,res)=>{
-    res.send("this is my about page of the app.");
+    res.send("this is my about page of the app made through express.");
+});
+
+app.get("/home" , (req,res)=>{
+    res.send("This is the h9ome page of the app.");
 });
 
 app.listen(port,()=>{
