763a10867d9eb28521bedf0840a881d24e3ca724
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-04-12 21:59:35 +0530

    learned to write html in pug page

--- Express/app.js
+++ Express/app.js
@@ -3,40 +3,21 @@ const path= require("path");
 const app = express();
 const port = 80;
 
-//for serving static files.
+//express related stuff.
 app.use('/static',express.static('static'));
 
-//set the template engien as pug.
+//pug related stuff.
 app.set('view engine','pug');
-
-//set the views directory.
 app.set('views' , path.join(__dirname,'views') )
 
-//our pug demo endpoint
-app.get('/', (req, res) => {
-    res.status(200).render('demo', { title: 'Hey there', message: 'Hello there and thanks for telling me how to use pug..!' });
-});
-
-app.get('/' , (req,res) =>{
-    res.status(200).render('all',{title:'css page' , message : 'this is the css file.' });
+//endpoints.
+app.get('/',(req,res)=>{
+    const con = "this is on the internet."; 
+    const params = {'title': 'pug is a good template engine.',"content" : con};
+    res.status(200).render('index.pug',params);
 })
 
-app.get("/" , (req,res)=>{
-    res.send("this is my first express app.");
-});
-
-app.get("/about" , (req,res)=>{
-    res.send("this is my about page of the app.");
-});
-
-app.post("/about" , (req,res)=>{
-    res.send("this is my about page of the app made through express.");
-});
-
-app.get("/home" , (req,res)=>{
-    res.send("This is the h9ome page of the app.");
-});
-
+//server started
 app.listen(port,()=>{
     console.log(`the app has successfully started on the port  ${port}`);
 })
