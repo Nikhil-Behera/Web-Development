16437dd35363510465a922506f19da77d30c5cea
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-05-14 23:29:56 +0530

    updated test.js and added a new index file.

--- Express/test.js
+++ Express/test.js
@@ -4,15 +4,15 @@ const app = express();
 const port = 80;
 
 
-app.use('/static',express.static('static'));
+app.use('/static',express.static('static'));    
 
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
 
 app.get('/',(req,res)=>{
-    const con= "this is on the internet";
-    const params = {'title': 'pug is a good template engine.',"content" : con};
-    res.status(200).render('index.pug');
+    const con= "lol";
+    const params = {'title': 'Test app.',"content" : con};
+    res.status(200).render('index.pug',params);
 })
 
 app.listen(port,()=>{
