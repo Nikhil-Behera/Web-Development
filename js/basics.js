c75204e31b2a79d78f8735bb5adcc425bb2309a7
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-06-26 21:36:50 +0530

    day1

---
+++ js/basics.js
@@ -0,0 +1,44 @@
+const name = "nikhil"
+var accountId=56594
+let email = "adbeu@gmail.com"
+// name = "sai"
+// console.log(name);
+email="wbdwud@gmail.com"
+// console.log(email)
+
+// console.table([name,accountId,email])
+
+/*datatypes  */
+let name1 = "hi"
+let age = 5
+let isLock = false
+// console.log(typeof isLock)
+
+// number conversion
+let id = "56";
+let id1 = "36djs"
+// console.log(typeof id)
+// console.log(typeof id1)
+// let valinid= Number(id);
+// let valinid1= Number(id1);
+// console.log(typeof valinid) 
+// console.log(typeof valinid1)   // NaN = Not a Number
+// console.log(valinid)
+// console.log(valinid1)
+
+let anyNum = 69;
+// console.log(typeof anyNum)
+let strNum = String(anyNum)
+// console.log(typeof strNum)
+
+//operations
+let val = 5
+let negVal = -val
+// console.log(val)
+// console.log(negVal)
+
+let str1 = "hi"
+let str2 = " name"
+let str3 = str1+str2
+// console.log(str3)
+
