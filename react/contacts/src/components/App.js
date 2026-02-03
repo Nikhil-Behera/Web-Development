e10038e89ebf576309cb1da528e881ba11814fa5
Nikhil Behera <NikhilBeheraTech@gmail.com>
2026-02-03 21:53:07 +0530

    only search part remaining for contacts

--- react/contacts/src/components/App.js
+++ react/contacts/src/components/App.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from "react";
+import api from "../api/contacts"
 import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import './App.css';
@@ -10,27 +11,47 @@ import ContactDetail from "./contactdetail";
 function App() {
   const LOCAL_STORAGE_KEY = "contacts"
   const [contacts,setContacts] = useState([])
+  const [searchterm,setsearchterm] = useState([])
 
-  const addContactHandler = (contact)=>{
-    console.log(contact)
-    setContacts([...contacts, { id: uuidv4(), ...contact }])
+  // Get all contacts from Get retrieveContacts
+  const retrieveContacts = async ()=>{
+    const response = await api.get("http://localhost:3006/contacts")
+    return response.data
   }
 
-  const removeContactHandler = ((id)=>{
+  const addContactHandler = async (contact)=>{
+    console.log(contact)
+    const request = {
+      id:uuidv4(),
+      ...contact
+    }
+    const response = await api.post("http://localhost:3006/contacts",request)
+    setContacts([...contacts, response.data])
+  }
+
+  const removeContactHandler = async (id)=>{
+    await api.delete(`http://localhost:3006/contacts/${id}`)
     const newcontactlist = contacts.filter((contact)=>{
       return contact.id != id
     })
 
     setContacts(newcontactlist)
-  })
+  }
+
+  const searchhandler = ()=>{}
 
   useEffect(()=>{
     localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts))
     },[contacts]
   )
   useEffect(()=>{
-    const retrieveitem = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if(retrieveitem) setContacts(retrieveitem)
+    // const retrieveitem = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    // if(retrieveitem) setContacts(retrieveitem)
+    const getAllContacts = async ()=> {
+      const getallcontacts = await retrieveContacts()
+      if(getallcontacts) setContacts(getallcontacts)
+    }
+    getAllContacts()
   },[])
 
   return (
@@ -46,7 +67,7 @@ function App() {
               <Route 
                 path="/home" 
                 exact
-                element={<ContactList contacts={contacts} getContact={removeContactHandler}/>}
+                element={<ContactList contacts={contacts} getContact={removeContactHandler} term={searchterm} searchkeyword={searchhandler}/>}
               />
               <Route
                 path="/contact/:id"
--- react/contacts/src/components/contactlist.js
+++ react/contacts/src/components/contactlist.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import {Link} from "react-router-dom";
 import ContactCard from './contactcard';
 
 const ContactList = (props)=>{
+    const inpele = useRef('')
 
     const renderContactList = props.contacts.map((contact) => {
         return(
@@ -14,12 +15,23 @@ const ContactList = (props)=>{
         props.getContact(id)
     }
 
+    const getsearchterm = ()=>{
+
+    }
+
     return(
         <>
             <div className='ui clearing segment'>
             <h2 className='ui left floated header'>
                 Contact List
             </h2>
+
+            <div className='ui search'>
+                <div className='ui icon input'>
+                    <i className='search icon'></i>
+                    <input type='text' placeholder='Search' className='prompt' value={props.term} onChange={getsearchterm}></input>
+                </div>
+            </div>
 
             <Link to={"/add"}>
                 <button className='ui button blue right floated'>Add Contact</button>
